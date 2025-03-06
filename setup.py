29d1be64d32502357dd9879e0a40a693f0918ab7
Peter Y. Yeh <pyeh@amd.com>
2025-03-06 10:44:29 -0800

    fix gpu_arch

--- setup.py
+++ setup.py
@@ -376,7 +376,7 @@ def get_extensions():
     # TOOD: Remove this and use what CUDA has once we fix all the builds.
     if IS_ROCM and use_cuda:
         # Add ROCm GPU architecture check
-        gpu_arch = torch.cuda.get_device_properties(0).name
+        gpu_arch = torch.cuda.get_device_properties(0).gcnArchName
         if gpu_arch != "gfx942":
             print(f"Warning: Unsupported ROCm GPU architecture: {gpu_arch}")
             print(
